a9c72650a4c7461c51d3d3f5f36dee9e2a98ed98
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-03-17 13:11:46 +0300

    Create pointer ile mutlak deger üzerinde matematiksel işlem.c

---
+++ pointer ile mutlak deger üzerinde matematiksel işlem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+void update(int *a,int *b);
+int main() {
+  int sayi1,sayi2;
+  printf("İki Sayi giriniz: ");
+  scanf("%d %d",&sayi1,&sayi2);
+  update(&sayi1,&sayi2);
+  return 0;
+}
+void update(int *a,int *b)
+{
+  int toplam=0;
+  int fark=0;
+  toplam=*a+*b;
+  fark=abs(*a-(*b));
+  printf("Sayinin Toplami : %d\n ",toplam);
+  printf("Sayinin Farki : %d ",fark);
+}
